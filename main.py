6d6f5c33b32d8f396b1d786f669f0b9cc67c9b2c
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-09 00:09:32 -0400

    added player and enemy class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 # importing necessary pygame local actions
 from pygame.locals import (
@@ -21,6 +21,45 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+    
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+        
+        # screen limitations
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        def __init__(self):
+            super(Enemy, self).__init__()
+            self.surf = pygame.Surface((20, 10))
+            self.surf.fill((255, 255, 255))
+            self.rect = self.surf.get_rect(
+                center = (
+                    random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                    random.randint(0, SCREEN_HEIGHT),
+                )
+            )
+            self.speed = random.randint(5, 20)
+
+        def update(self):
+            self.rect.move_ip(-self.speed, 0)
+            if self.rect.right < 0:
+                self.kill()
 
 pygame.init()
 
@@ -39,10 +78,17 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    # white background
+    pressed_keys = pygame.key.get_pressed()
+
+    player.update(pressed_keys)
+
+    # black background
     screen.fill((0, 0, 0))
     screen.blit(player.surf, player.rect)
     pygame.display.flip()
+
+
+
 
 # game surface
 # screen.fill((255, 255, 255))
@@ -59,7 +105,6 @@ while running:
 # pygame.display.flip()
 
 # screen.fill((255, 255, 255))
-
 
 
 
