46fcfdf7552f1755114c337f74863e7a79610de3
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-08 23:57:39 -0400

    setup canvas

--- main.py
+++ main.py
@@ -10,22 +10,58 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# player object
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
 pygame.init()
 
 # drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
 
 # set game active until user quits
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
             running = False
+
     # white background
-    screen.fill((255, 255, 255))
-
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
+    screen.fill((0, 0, 0))
+    screen.blit(player.surf, player.rect)
     pygame.display.flip()
+
+# game surface
+# screen.fill((255, 255, 255))
+# surf = pygame.Surface((50, 50))
+# suft.fill((0, 0, 0))   # color
+# rect = surf.get_srect()
+
+# pygame.display.flip()
+# surf_center = (
+#     (SCREEN_WIDTH-surf.get_width())/2,
+#     (SCREEN_HEIGHT-surf.get_height())/2
+# )
+# screen.blit(surf, surf_center)    # center
+# pygame.display.flip()
+
+# screen.fill((255, 255, 255))
+
+
+
+
 
 pygame.quit()
